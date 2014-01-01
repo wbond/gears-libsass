fad6b6408fd01d7d6beccd19cb1cec1185960705
wbond <will@wbond.net>
2013-12-31 21:31:06 -0500

    Fixed include folder so imports

--- gears_scss/compiler.py
+++ gears_scss/compiler.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import sass
 from gears.compilers import BaseCompiler
 
@@ -8,7 +9,10 @@ class SCSSCompiler(BaseCompiler):
     result_mimetype = 'text/css'
 
     def __call__(self, asset):
-        asset.processed_source = self.run(asset.processed_source)
+        include_folder = os.path.dirname(asset.absolute_path)
+        asset.processed_source = self.run(asset.processed_source, include_folder)
 
-    def run(self, source):
-        return sass.compile_string(source.encode('utf-8')).decode('utf-8')
+    def run(self, source, include_folder):
+        source = bytes(source, encoding='utf-8')
+        include_folder = include_folder.encode(sys.getfilesystemencoding())
+        return sass.compile_string(source, include_folder).decode('utf-8')
